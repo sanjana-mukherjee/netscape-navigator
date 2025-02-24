b87d3303e7e4416777e8e8e78a8c859273da1b4c
sanjana-mukherjee <sanjana2mukherjee@gmail.com>
2025-02-24 19:30:18 +0530

    setup sections structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Content from "./components/sections/Content";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <div className="p-10">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="p-4 sm:p-16 font-gsans">
+      <h1 className="text-center text-2xl font-light border-b border-b-black/20 uppercase">
+        Netscape to Firefox
+      </h1>
+      <div className="mt-24">
+        <Content />;
       </div>
-      <h1 className="text-4xl">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
-
-export default App;

b90b5a2289bcfd27eee711273cc55301b90ab5cd
sanjana-mukherjee <sanjana2mukherjee@gmail.com>
2025-02-27 15:54:22 +0530

    update top padding

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Content from "./components/sections/Content";
 
 export default function App() {
   return (
-    <div className="p-4 sm:p-16 font-gsans">
+    <div className="p-16 pt-2 font-gsans">
       <h1 className="text-center text-2xl font-light border-b border-b-black/20 uppercase">
         Netscape to Firefox
       </h1>

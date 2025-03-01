664c33d95e817fafb6b7ace1c894eb80dbfe5a07
sanjana-mukherjee <sanjana2mukherjee@gmail.com>
2025-03-01 13:14:11 +0530

    fix styling

--- src/components/sections/SubSection.tsx
+++ src/components/sections/SubSection.tsx
@@ -7,7 +7,7 @@ export default function SubSection({
 }) {
   return (
     <div className="flex gap-20 justify-between items-start">
-      <h3 className="text-5xl font-normal mb-8 col-span-2 font-melodrama sticky top-4">
+      <h3 className="max-w-md text-5xl font-normal mb-8 col-span-2 font-melodrama sticky top-4">
         {heading}
       </h3>
       <div className="text-justify max-w-prose space-y-5 font-satoshi text-xl col-start-2 font-light">

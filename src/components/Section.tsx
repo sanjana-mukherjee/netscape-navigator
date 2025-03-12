e0c5d0c0e00c5ed730d1d78e38263896b5352e67
Sanjana Mukherjee <sanjana2mukherjee@gmail.com>
2025-03-12 20:41:57 +0530

    fix Icon component

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -17,8 +17,13 @@ export default function Section({
         <h2 className="text-9xl uppercase font-semibold font-melodrama max-w-2xl">
           {heading}
         </h2>
-        <div className="shrink-0 opacity-30">
-          <Icon absoluteStrokeWidth={true} strokeWidth={1} size={160} />
+        <div className="shrink-0" aria-hidden="true">
+          <Icon
+            absoluteStrokeWidth={true}
+            strokeWidth={1}
+            size={240}
+            color="#dbdbdb"
+          />
         </div>
       </div>
       <div className="mt-30 pb-18 space-y-36 relative before:absolute before:inset-y-0 before:left-4 before:right-0 before:-z-10 before:border-l before:border-l-black/20">

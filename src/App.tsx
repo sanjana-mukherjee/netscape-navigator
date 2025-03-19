9cc7a3c5b91c2ddf222787e8b5ef9bdb965586a7
Sanjana Mukherjee <sanjana2mukherjee@gmail.com>
2025-03-19 16:33:54 +0530

    update css styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Content from "./components/Content";
 export default function App() {
   return (
     <main className="font-gsans">
-      <div className="pt-1 bg-linear-[var(--color-gray-100)_65%,transparent_65%] flex items-center justify-center">
-        <h1 className="whitespace-nowrap uppercase text-4xl font-melodrama font-medium">
+      <div className="pt-1 bg-linear-[var(--color-gray-50)_65%,transparent_65%]">
+        <h1 className="text-center uppercase text-4xl font-melodrama font-medium">
           Netscape to firefox
         </h1>
       </div>

b5c675e4e7b023d3e0d40bb0e3901011d88ee208
Sanjana Mukherjee <sanjana2mukherjee@gmail.com>
2025-03-31 12:13:27 +0530

    add responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ export default function App() {
   return (
     <main className="font-gsans">
       <div className="pt-1 bg-linear-[var(--color-gray-50)_65%,transparent_65%]">
-        <h1 className="text-center uppercase text-4xl font-melodrama font-medium">
+        <h1 className="text-center uppercase font-melodrama font-medium px-2 text-2xl md:text-4xl">
           Netscape to firefox
         </h1>
       </div>
-      <div className="mt-48">
+      <div className="mt-10 sm:mt-28 md:mt-48">
         <Content />
       </div>
     </main>

82e8f50974e6bd3dba28d3a872beb059a6f2d02b
Sanjana Mukherjee <sanjana2mukherjee@gmail.com>
2025-04-03 22:02:58 +0530

    add responsiveness

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -13,8 +13,8 @@ export default function Section({
       <Container variant="large">
         <InteractiveBackground>
           <Container>
-            <div className="py-15">
-              <h2 className="text-pretty bg-radial from-white/70 via-white/50 to-transparent text-9xl uppercase font-semibold font-melodrama max-w-3xl">
+            <div className="py-10 md:py-15">
+              <h2 className="text-pretty bg-radial from-white/70 via-white/50 to-transparent uppercase font-semibold font-melodrama text-6xl max-w-sm sm:text-8xl sm:max-w-xl md:text-9xl md:max-w-3xl">
                 <div className="bg-radial from-black/65 to-black bg-clip-text text-transparent">
                   {heading}
                 </div>
@@ -24,7 +24,7 @@ export default function Section({
         </InteractiveBackground>
       </Container>
       <Container>
-        <div className="mt-20 pb-18 space-y-36 relative before:absolute before:inset-y-0 before:left-4 before:border-l before:border-l-black/20">
+        <div className="mt-10 sm:mt-20 pb-10 sm:pb-18 space-y-28 lg:space-y-36 relative before:absolute before:inset-y-0 before:left-0 lg:before:left-4 before:border-l before:border-l-gray-300">
           {children}
         </div>
       </Container>

0b62ff582982218370b3e8c87a6dc9fad4abf91c
Sanjana Mukherjee <sanjana2mukherjee@gmail.com>
2025-04-09 15:28:31 +0530

    update timeline styles

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,15 +1,3 @@
-import {
-  Clock2,
-  Clock3,
-  Clock4,
-  Clock5,
-  Clock7,
-  Clock8,
-  Clock9,
-} from "lucide-react";
-
-const ClockIcons = [Clock2, Clock3, Clock4, Clock5, Clock7, Clock8, Clock9];
-
 export function Timeline({ children }: { children: React.ReactNode }) {
   return <dl className="space-y-2 lg:space-y-7">{children}</dl>;
 }
@@ -21,20 +9,13 @@ export function TimelineItem({
   year: number;
   children: React.ReactNode;
 }) {
-  const Clock = ClockIcons[year % ClockIcons.length];
-
   return (
     <>
-      <div className="-ml-[0.5px] hidden lg:block">
-        <div className="flex items-center gap-3">
-          <div className="-translate-x-1/2 bg-white py-1" aria-hidden>
-            <Clock size={24} strokeWidth={0.5} absoluteStrokeWidth={true} />
-          </div>
-          <dd className="font-melodrama text-gradient text-2xl font-semibold tracking-wide">
-            {year}
-          </dd>
-        </div>
-        <dt className="col-start-2 ml-9 text-sm">{children}</dt>
+      <div className="hidden pl-12 lg:block">
+        <dd className="font-melodrama text-gradient relative text-2xl font-semibold tracking-wide before:absolute before:top-1/2 before:-left-11.5 before:w-9 before:border-t before:border-gray-400 after:absolute after:top-1/2 after:-left-13.5 after:mt-[0.5px] after:ml-[1.5px] after:size-3 after:-translate-y-1/2 after:rounded-full after:border after:border-gray-400 after:bg-gray-100">
+          {year}
+        </dd>
+        <dt className="col-start-2 text-sm">{children}</dt>
       </div>
       <div className="flex items-start gap-3 text-sm leading-tight lg:hidden lg:leading-normal">
         <dd className="relative font-normal before:absolute before:top-1/2 before:left-0 before:w-8 before:-translate-x-10 before:-translate-y-1/2 before:border-t before:border-t-gray-300 after:absolute after:top-1/2 after:left-0 after:size-2 after:-translate-x-11 after:-translate-y-1/2 after:rounded-full after:border after:border-gray-300 after:bg-gray-100">

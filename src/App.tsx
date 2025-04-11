25b5361fc173a1bef7b597d7893e52a75e23b829
Sanjana Mukherjee <sanjana2mukherjee@gmail.com>
2025-04-11 15:45:12 +0530

    add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import Container from "./components/Container";
 import Content from "./components/Content";
 import Conclusion from "./components/sections/Conclusion";
+import Footer from "./components/sections/Footer";
 import Introduction from "./components/sections/Introduction";
 
 export default function App() {
   return (
-    <main className="font-gsans bg-gray-50">
-      <Container variant="large">
-        <div className="bg-white pt-px">
-          <Introduction />
-          <div className="mt-18 sm:mt-28 md:mt-48">
-            <Content />
-          </div>
+    <>
+      <main className="font-gsans bg-gray-50">
+        <Container variant="large">
+          <div className="bg-white pt-px">
+            <Introduction />
+            <div className="mt-18 sm:mt-28 md:mt-48">
+              <Content />
+            </div>
             <Conclusion />
-        </div>
-      </Container>
-    </main>
+          </div>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,53 @@
+import { Linkedin, Mail, Twitter } from "lucide-react";
+import Container from "../Container";
+import InteractiveBackground from "../InteractiveBackground";
+
+export default function Footer() {
+  return (
+    <InteractiveBackground>
+      <Container>
+        <div className="mt-20 flex flex-col items-start justify-between gap-10 py-20 sm:flex-row">
+          <div>
+            <p className="font-melodrama text-gradient text-6xl font-semibold text-pretty uppercase">
+              Thank You for Reading!
+            </p>
+            <p className="mt-6 max-w-prose">
+              <span className="bg-white">
+                Hey, I'm <strong>Sanjana</strong>! I'm a{" "}
+                <strong>frontend developer</strong> who enjoys sharing cool
+                stuff about the web through <strong>blog posts</strong> and{" "}
+                <strong>videos</strong>.
+              </span>
+            </p>
+          </div>
+          <div className="flex space-x-2">
+            <a
+              href="https://x.com/__mukherjee"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded border border-sky-100 bg-sky-50 p-1.5 text-black hover:border-sky-200"
+            >
+              <Twitter />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/mkhrj/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded border border-sky-100 bg-sky-50 p-1.5 text-black hover:border-sky-200"
+            >
+              <Linkedin />
+            </a>
+            <a
+              href="mailto:dev.sanjanamukherjee@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded border border-sky-100 bg-sky-50 p-1.5 text-black hover:border-sky-200"
+            >
+              <Mail />
+            </a>
+          </div>
+        </div>
+      </Container>
+    </InteractiveBackground>
+  );
+}

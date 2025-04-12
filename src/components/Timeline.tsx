9522ee04ad542a60154207d08a296ee2a69b762b
Sanjana Mukherjee <sanjana2mukherjee@gmail.com>
2025-04-12 19:17:55 +0530

    update timeline styles

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -12,13 +12,13 @@ export function TimelineItem({
   return (
     <>
       <div className="hidden pl-12 lg:block">
-        <dd className="font-melodrama text-gradient relative text-2xl font-semibold tracking-wide before:absolute before:top-1/2 before:-left-11.5 before:w-9 before:border-t before:border-gray-400 after:absolute after:top-1/2 after:-left-13.5 after:mt-[0.5px] after:ml-[1.5px] after:size-3 after:-translate-y-1/2 after:rounded-full after:border after:border-gray-400 after:bg-sky-50">
+        <dd className="font-melodrama text-gradient relative text-2xl font-semibold tracking-wide before:absolute before:top-1/2 before:-left-11.5 before:w-9 before:border-t before:border-gray-400 after:absolute after:top-1/2 after:-left-13 after:size-2.5 after:-translate-y-1/2 after:rotate-45 after:border after:border-gray-400 after:bg-sky-50">
           {year}
         </dd>
         <dt className="col-start-2 text-sm">{children}</dt>
       </div>
       <div className="flex items-start gap-3 text-sm leading-tight lg:hidden lg:leading-normal">
-        <dd className="relative font-normal before:absolute before:top-1/2 before:left-0 before:w-8 before:-translate-x-10 before:-translate-y-1/2 before:border-t before:border-t-gray-300 after:absolute after:top-1/2 after:left-0 after:size-2 after:-translate-x-11 after:-translate-y-1/2 after:rounded-full after:border after:border-gray-300 after:bg-sky-50">
+        <dd className="relative font-normal before:absolute before:top-1/2 before:left-0 before:w-8 before:-translate-x-10 before:-translate-y-1/2 before:border-t before:border-t-gray-300 after:absolute after:top-1/2 after:left-0 after:size-2 after:-translate-x-11 after:-translate-y-1/2 after:rotate-45 after:border after:border-gray-300 after:bg-sky-50">
           {year}
         </dd>
         <dt>{children}</dt>
